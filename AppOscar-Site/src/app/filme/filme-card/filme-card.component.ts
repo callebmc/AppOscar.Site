f406d7b20d7f6064e4f8e50759893c44a39368be
CallebCecco <calleb.malinoski@gmail.com>
2020-01-31 00:04:07 -0300

    Adiciona component de Card de Filme

---
+++ AppOscar-Site/src/app/filme/filme-card/filme-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filme-card',
+  templateUrl: './filme-card.component.html'
+})
+export class FilmeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

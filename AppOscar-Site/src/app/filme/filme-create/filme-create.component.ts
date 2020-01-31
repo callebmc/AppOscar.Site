8912687c379172bc5e49a36a2a2eb9c067de7ed3
CallebCecco <calleb.malinoski@gmail.com>
2020-01-31 00:04:25 -0300

    Adiciona component de Create Filme

---
+++ AppOscar-Site/src/app/filme/filme-create/filme-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filme-create',
+  templateUrl: './filme-create.component.html',
+  styleUrls: ['./filme-create.component.css']
+})
+export class FilmeCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

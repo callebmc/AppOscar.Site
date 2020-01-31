cd229b7c55fafedbbf6d3d3e1ae8edf67739eefd
CallebCecco <calleb.malinoski@gmail.com>
2020-01-31 00:37:36 -0300

    Adiciona navegação até listagem de filmes

--- AppOscar-Site/src/app/routes.ts
+++ AppOscar-Site/src/app/routes.ts
@@ -1,25 +1,37 @@
-import { Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { CategoriaListComponent } from "./categoria/categoria-list/categoria-list.component";
-import { CategoriaListResolver } from "./_resolvers/categoria-list.resolver";
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoriaListComponent } from './categoria/categoria-list/categoria-list.component';
+import { CategoriaListResolver } from './_resolvers/categoria-list.resolver';
 import { CategoriaCreateComponent } from './categoria/categoria-create/categoria-create.component';
+import { FilmeListComponent } from './filme/filme-list/filme-list.component';
+import { FilmeCreateComponent } from './filme/filme-create/filme-create.component';
+import { FilmeListResolver } from './_resolvers/filme-list.resolver';
 
 export const appRoutes: Routes = [
-    { path: "", component: HomeComponent },
+    { path: '', component: HomeComponent },
     {
-        path: "",
-        runGuardsAndResolvers: "always",
+        path: '',
+        runGuardsAndResolvers: 'always',
         children: [
             {
-                path: "categorias",
+                path: 'categorias',
                 component: CategoriaListComponent,
                 resolve: { categoria: CategoriaListResolver }
             },
             {
-                path: "categoria/criar",
+                path: 'categoria/criar',
                 component: CategoriaCreateComponent
+            },
+            {
+                path: 'filme',
+                component: FilmeListComponent,
+                resolve: { filme: FilmeListResolver }
+            },
+            {
+                path: 'filme/criar',
+                component: FilmeCreateComponent
             }
         ]
     },
-    { path: "**", redirectTo: "", pathMatch: "full" }
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];

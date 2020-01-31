44967593420f68a005f03d365ed51b12e481dbca
CallebCecco <calleb.malinoski@gmail.com>
2020-01-31 00:04:55 -0300

    Adiciona component de Listagem de FIlmes

---
+++ AppOscar-Site/src/app/filme/filme-list/filme-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filme-list',
+  templateUrl: './filme-list.component.html'
+})
+export class FilmeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

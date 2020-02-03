17eed69d52f8a8a714e46ddae34303a419cd6509
CallebCecco <calleb.malinoski@gmail.com>
2020-02-03 17:17:31 -0300

    Adiciona services novos

---
+++ AppOscar-Site/src/app/_services/participacao.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Participacao } from "../_models/participacao";
+import {
+    JsonDeserializer,
+    JsonDeserializerFactory
+} from "./json-deserialize.service";
+import { Filme } from "../_models/filme";
+import { Categoria } from "../_models/categoria";
+import { concatMap, toArray } from "rxjs/operators";
+import { of, concat } from "rxjs";
+
+@Injectable({
+    providedIn: "root"
+})
+export class ParticipacaoService {
+    baseUrl = environment.apiUrl;
+
+    deserialize: JsonDeserializer;
+    deserializeFilme: JsonDeserializer;
+    deserializeCategoria: JsonDeserializer;
+
+    constructor(
+        private http: HttpClient,
+        private jsonFactory: JsonDeserializerFactory
+    ) {
+        this.deserialize = this.jsonFactory.construct(Participacao);
+        this.deserializeFilme = this.jsonFactory.construct(Filme);
+        this.deserializeCategoria = this.jsonFactory.construct(Categoria);
+    }
+
+    getParticipacaoByCategoria(categoria: Categoria) {
+        const url = `participacao/by-categoria/${categoria.idCategoria}`;
+        return this.http.get<Filme[]>(this.baseUrl + url).pipe(
+            concatMap(filmes => {
+                const deserialized = this.deserializeFilme.deserialize(
+                    filmes
+                ) as Array<Filme>;
+                categoria.filmes = deserialized;
+                return deserialized;
+            }),
+            concatMap(filmes => of(filmes)),
+            concatMap(filmes => {
+                return of(filmes);
+            }),
+            toArray()
+        );
+    }
+}

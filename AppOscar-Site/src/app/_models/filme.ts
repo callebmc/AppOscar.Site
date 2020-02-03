d3f32622a6cb205c213258290029b737e0d49697
CallebCecco <calleb.malinoski@gmail.com>
2020-02-03 17:17:10 -0300

    Adiciona novos modelos e propriedades

--- AppOscar-Site/src/app/_models/filme.ts
+++ AppOscar-Site/src/app/_models/filme.ts
@@ -1,4 +1,18 @@
-export interface Filme {
-    id: number;
+import { Participacao } from './participacao';
+import { Type } from 'class-transformer';
+import { Categoria } from './categoria';
+
+export class Filme {
+
+    id: string;
+
     nomeFilme: string;
+
+    // @Type(() => Participacao)
+    participantes: Participacao;
+
+    // @Type(() => Categoria)
+    categoriasVencidas: Categoria;
+
+    filmePhotoUrl: string;
 }
